467e19bdb3a936df64a3a4654923810f1b83dcec
Taylor Jadin <jadin.taylor@gmail.com>
2020-08-11 22:20:44 -0500

    Update login-form-custom-css.php

--- login-form-custom-css.php
+++ login-form-custom-css.php
@@ -11,9 +11,9 @@ function login_form_custom_css() {
     echo '<style type="text/css">
         #login > h1 {display: none;}
         #login > p.message {display: none;}
-	    #loginform > p:nth-child(1) {display: none;}
-	    #loginform > div.user-pass-wrap {display: none;}
-	    #loginform > p.forgetmenot {display: none;}
+	#loginform > p:nth-child(1) {display: none;}
+	#loginform > div.user-pass-wrap {display: none;}
+	#loginform > p.forgetmenot {display: none;}
         #loginform > p.submit {display: none;}
         #nsl-custom-login-form-main .nsl-container-login-layout-below {padding: 0px !important;}
         #nav {display: none;}
