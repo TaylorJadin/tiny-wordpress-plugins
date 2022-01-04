18be1c90639766df23621dbbb48a7f8219e0eace
Taylor Jadin <jadin.taylor@gmail.com>
2022-01-03 20:40:10 -0600

    Update dashboard-widget.php

--- dashboard-widget.php
+++ dashboard-widget.php
@@ -7,19 +7,18 @@
 * Author: Taylor Jadin
 * Author URI: https://jadin.me/
 **/
-add_action('wp_dashboard_setup', 'my_custom_dashboard_widgets');
-  
-function my_custom_dashboard_widgets() {
-global $wp_meta_boxes;
- 
-wp_add_dashboard_widget('custom_help_widget', 'custom_dashboard_title', 'custom_dashboard_text');
-}
 
-function custom_dashboard_title() {
-  echo 'Getting Started';
+$widget_title = 'Getting started with your Domains Community site!';
+$widget_text = '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>';
+
+add_action('wp_dashboard_setup', 'my_custom_dashboard_widgets');
+function my_custom_dashboard_widgets() {
+global $wp_meta_boxes, $widget_title;
+wp_add_dashboard_widget('custom_help_widget', $widget_title, 'custom_dashboard_text');
 }
 
 function custom_dashboard_text() {
-echo '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>';
+global $widget_text;
+echo $widget_text;
 }
 ?>
