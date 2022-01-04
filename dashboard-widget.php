313fb3b32aafcae0173a74f000dd36958eb541ed
Taylor Jadin <jadin.taylor@gmail.com>
2022-01-03 20:40:44 -0600

    Update dashboard-widget.php

--- dashboard-widget.php
+++ dashboard-widget.php
@@ -2,7 +2,7 @@
 /**
 * Plugin Name: Dashboard Widget
 * Plugin URI: https://github.com/TaylorJadin/tiny-wordpress-plugins
-* Description: This plugin adds a simple text widget to the dashboard. Based on: https://www.wpbeginner.com/wp-themes/how-to-add-custom-dashboard-widgets-in-wordpress/
+* Description: This plugin adds a simple text widget to the dashboard.
 * Version: 1.0
 * Author: Taylor Jadin
 * Author URI: https://jadin.me/
