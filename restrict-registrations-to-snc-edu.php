763c9e04240d94ac342c223e53181a1052ad7b61
Taylor Jadin <jadin.taylor@gmail.com>
2022-06-22 14:52:55 -0500

    descrip change

--- restrict-registrations-to-snc-edu.php
+++ restrict-registrations-to-snc-edu.php
@@ -1,16 +1,16 @@
 <?php
 /**
-* Plugin Name: Restrict registrations to snc.edu
+* Plugin Name: Restrict registrations to email domain
 * Plugin URI: https://github.com/TaylorJadin/tiny-wordpress-plugins
-* Description: This plugin makes it so you need an @snc.edu email to register an account on this site.
+* Description: This plugin makes it so you need an @reclaimhosting.com email to register an account on this site.
 * Version: 1.0
 * Author: Taylor Jadin
 * Author URI: https://jadin.me/
 **/
 
-// Restrict registration to snc.edu
+// Restrict registration to specific domain
 function is_valid_email_domain($login, $email, $errors ){
- $valid_email_domains = array("snc.edu");// allowed domains
+ $valid_email_domains = array("reclaimhosting.com");// allowed domains
  $valid = false; // sets default validation to false
  foreach( $valid_email_domains as $d ){
   $d_length = strlen( $d );
