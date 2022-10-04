094d06400519fabda07fa4b86b875cce357fc4a7
Taylor Jadin <jadin.taylor@gmail.com>
2022-10-04 10:52:10 -0500

    clean up

---
+++ blur-user-info.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* Plugin Name: Blur User Info
+* Plugin URI: https://github.com/TaylorJadin/tiny-wordpress-plugins
+* Description: Adds CSS to blue user info in the Users view. Idea and CSS from: https://bionicteaching.com/handy-css-for-wp-presentation-privacy/
+* Version: 1.0
+* Author: Taylor Jadin
+* Author URI: https://jadin.me/
+**/
+function blur_user_info() {
+        echo '<style type="text/css">
+        .email a, .column-username strong, td.column-name {
+                filter: blur(4px);
+              }
+	</style>';
+}
+add_action('admin_head', 'blur_user_info');
+?>
