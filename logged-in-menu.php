30ffee6ea3412ea2d6db1bc381ef131a9b08e198
Taylor Jadin <jadin.taylor@gmail.com>
2024-04-08 09:32:26 -0500

    Create logged-in-menu.php

---
+++ logged-in-menu.php
@@ -0,0 +1,20 @@
+<?php
+/*
+* Plugin Name: Logged In and Logged Out Menu
+* Description: Site specific code changes for knight.domains that allow us to have a different menu depending on wether you are logged in or out
+* Version: 1.0
+* Author: Taylor Jadin
+* Author URI: https://jadin.me/
+*/
+/* Start Adding Functions Below this Line */
+function my_wp_nav_menu_args( $args = '' ) {
+ 
+    if( is_user_logged_in() ) { 
+        $args['menu'] = 'logged-in';
+    } else { 
+        $args['menu'] = 'logged-out';
+    } 
+        return $args;
+    }
+    add_filter( 'wp_nav_menu_args', 'my_wp_nav_menu_args' );
+?>
